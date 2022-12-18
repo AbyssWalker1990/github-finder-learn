e07ede295de5f35eecd3110ab427000b635cb44b
vova <bordiian.v@gmail.com>
2022-12-18 15:33:37 +0300

    created basic pages and add routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-6">About Page</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum ex porro numquam sit deleniti esse magnam nulla quasi obcaecati, explicabo, veniam quidem molestiae in laboriosam totam libero iusto eos eum nostrum distinctio. Beatae maiores debitis, consectetur dolorum et magni quis laboriosam. Accusantium velit laudantium quisquam delectus deleniti vel quas iste reprehenderit eum corporis beatae at eos magni voluptatem distinctio sit eveniet minima sunt laboriosam, quis aut ullam a error illo. Repellat praesentium dicta natus at accusantium rerum ipsum, blanditiis ullam ad. Id in quas quia dolorem, laudantium explicabo ipsum blanditiis nihil ab adipisci ut corporis soluta omnis? Quas, praesentium.</p>
+    </div>
+  )
+}
+
+export default About
