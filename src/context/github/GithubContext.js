089902d33cf9ec9d320239de21c98a072efee693
vova <bordiian.v@gmail.com>
2022-12-22 19:14:02 +0300

    refactored getser and getepos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -2,8 +2,6 @@ import { createContext, useReducer } from "react";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 
 export const GithubProvider = ({children}) => {
   const initialState = {
@@ -15,68 +13,9 @@ export const GithubProvider = ({children}) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-
-
-    // Get single user
-    const getUser = async (login) => {
-      setLoading()
-  
-      const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`
-        }
-      })
-
-      if (response.status === 404) {
-        window.location = '/notfound'
-      } else {
-        const data = await response.json()
-      
-        dispatch({
-          type: 'GET_USER',
-          payload: data,
-        })
-      }
-    }
-
-  // Get repos
-  const getUserRepos = async (login) => {
-    setLoading()
-
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10,
-    })
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`
-      }
-    })
-
-    const data = await response.json()
-    
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data
-    })
-  }
-
-  const clearUsers = () => {
-    dispatch({
-      type: 'CLEAR_USERS'
-    })
-  }
-
-  // function for set loading
-  const setLoading = () => dispatch({type: 'SET_LOADING'})
-
   return <GithubContext.Provider value={{
     ...state,
     dispatch,
-    clearUsers,
-    getUser,
-    getUserRepos,
   }}>
     {children}
   </GithubContext.Provider>
